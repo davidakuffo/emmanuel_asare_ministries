db7e9bf0b37dbe3507b4dcc82461792cf0b4b44f
David Offei Akuffo <david.akuffo11@gmail.com>
2024-07-31 00:02:17 +0000

    change of name

---
+++ src/app/OurContacts/Navbar.tsx
@@ -0,0 +1,17 @@
+const NavBar = () => (
+  <nav
+    className="bg-sky-400 text-white
+  p-4"
+  >
+    <ul className="flex justify-between">
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Sermons</li>
+      <li>Events</li>
+      <li>Contact Us</li>
+      <button className="bg-amber-300 text-black px-4 py-2 rounded-[1rem]"></button>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
